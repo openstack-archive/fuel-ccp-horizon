e9a60021fb3f8742b750fcb5c667dbc842894f10
Artur Zarzycki <azarzycki@mirantis.com>
2016-05-19 11:37:31 +0200

    Add horizon service definition for k8s
    Change-Id: I379620e05ebc82cf26aaf63f84d5c58120cb48a2

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-/var/lib/microservices/venv/bin/python /var/lib/microservices/venv/bin/manage.py compress --force
+/var/lib/microservices/venv/bin/python \
+    /var/lib/microservices/venv/bin/manage.py compress --force
 
 source /etc/apache2/envvars
 
-sed -i "s/Secret_String/${HORIZON_SECRET_KEY}" /etc/openstack-dashboard/local_settings
-sed -i "s/KEYSTONE_ADDRESS/${KEYSTONE_ADDRESS}" /etc/openstack-dashboard/local_settings
+sed -i "s/Secret_String/${HORIZON_SECRET_KEY}" \
+    /etc/openstack-dashboard/local_settings
+sed -i "s/KEYSTONE_ADDRESS/${KEYSTONE_ADDRESS}" \
+    /etc/openstack-dashboard/local_settings
+
+#check does keyston is alive
+nc -z -v -w5 ${KEYSTONE_ADDRESS} 5000
+if [ $? -ne 0 ];then
+    exit 1
+fi
+
 
 exec /usr/sbin/apache2 -DNO_DETACH
