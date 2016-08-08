4a95848c85cd531b93353fdc37887df20c806587
Andrey <apavlov@mirantis.com>
2016-08-08 20:25:19 +0000

    [DO NOT MERGE] Add vendor theme
    Change-Id: I8438519bd62fabfcc76d35088f0156e2345dfef1

---
+++ docker/horizon/_10_set_custom_theme.py
@@ -0,0 +1,3 @@
+# override the CUSTOM_THEME_PATH variable with this settings snippet
+AVAILABLE_THEMES = [('default', 'Default', 'themes/default'),
+                    ('mirantis', 'Mirantis', 'themes/vendor')]
